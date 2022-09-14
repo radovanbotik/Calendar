df15fa309e3f33e2f5c116f44248bbfe22252fb5
radovanbotik <radovanbotik@gmail.com>
2022-09-14 16:27:20 +0200

    number of days in sync with the current month

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-const months = document.querySelectorAll(".month");
-const chevronRight = document.querySelector(".chevron_right");
-const chevronLeft = document.querySelector(".chevron_left");
-months.forEach((month, index) => {
-  month.style.left = `${index * 100}%`;
-});
-let counter = 0;
-const translateX = counter => {
-  months.forEach((month, index) => {
-    month.style.transform = `translateX(-${counter * 100}%)`;
-  });
-};
-
-chevronRight.addEventListener("click", e => {
-  counter++;
-  if (counter > months.length - 1) {
-    counter = 0;
-  }
-  translateX(counter);
-  console.log(counter);
-});
-chevronLeft.addEventListener("click", e => {
-  counter--;
-  if (counter < 0) {
-    counter = months.length - 1;
-  }
-  translateX(counter);
-  console.log(counter);
-});
-
-const date = new Date(2022, 0, 1);
-const days2022 = [];
-for (let i = 1; i <= 365; i++) {
-  days2022.push(new Date(2022, 0, `${i}`));
-}
-
 const allmonths = [
   "january",
   "february",
@@ -48,6 +12,32 @@ const allmonths = [
   "november",
   "december",
 ];
+const months = document.querySelectorAll(".month");
+const chevronRight = document.querySelector(".chevron_right");
+const chevronLeft = document.querySelector(".chevron_left");
+months.forEach((month, index) => {
+  month.style.left = `${index * 100}%`;
+});
+const checkCurrentMonth = () => {
+  const currentMonth = new Date().getMonth();
+  return currentMonth;
+};
+let counter = checkCurrentMonth();
+
+const translateX = counter => {
+  months.forEach(month => {
+    month.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
+const daysInCurrentMonth = () => {
+  return wholeYear[counter];
+};
+
+const date = new Date(2022, 0, 1);
+const days2022 = [];
+for (let i = 1; i <= 365; i++) {
+  days2022.push(new Date(2022, 0, `${i}`));
+}
 
 const wholeYear = allmonths.map((month, index) => {
   return (month = new Array(
@@ -57,8 +47,42 @@ const wholeYear = allmonths.map((month, index) => {
   ));
 });
 
-const checkCurrentMonth = () => {
-  const currentMonth = new Date().getMonth();
-  console.log(wholeYear[currentMonth]);
+const createDays = () => {
+  const daysContainer =
+    months[counter].parentElement.parentElement.nextElementSibling
+      .nextElementSibling;
+  const currentMonthDays = daysInCurrentMonth()[0];
+  const daysHTML = currentMonthDays
+    .map((day, index) => {
+      return `
+    <li class="day">${index + 1}</li>
+    `;
+    })
+    .join("");
+  console.log(daysHTML);
+  daysContainer.innerHTML = daysHTML;
 };
-checkCurrentMonth();
+
+chevronRight.addEventListener("click", e => {
+  counter++;
+  if (counter > months.length - 1) {
+    counter = 0;
+  }
+  translateX(counter);
+  createDays();
+  console.log(daysInCurrentMonth());
+});
+chevronLeft.addEventListener("click", e => {
+  counter--;
+  if (counter < 0) {
+    counter = months.length - 1;
+  }
+  translateX(counter);
+  createDays();
+  console.log(daysInCurrentMonth());
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  translateX(counter);
+  createDays();
+});
